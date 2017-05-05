fc732421757315ab833a696ecaec6768153911a2
Erik Figueiredo <erik.figueiredo@gmail.com>
2017-05-05 20:27:01 -0300

    administraçao movida e estrutura base para o front criada

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,10 @@
 |
 */
 
-Route::get('/{vue?}', function () {
-    return view('app');
-})->where('vue', '[\/\w\.-]*')->name('home');
+Route::get('/', function () {
+    return view('home');
+})->name('home');
+
+Route::get('/dashboard/{vue?}', function () {
+    return view('admin');
+})->where('vue', '[\/\w\.-]*')->name('dashboard');
