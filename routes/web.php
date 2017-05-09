e61270f9d8efe64588bafffb533ba72907ce9d97
Erik Figueiredo <erik.figueiredo@gmail.com>
2017-05-09 11:38:36 -0300

    criadas as rotas do front

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,26 @@ Route::get('/', function () {
     return view('home');
 })->name('home');
 
+Route::get('/cadastro', function () {
+    // TODO: criar layout da página
+})->name('home');
+
+Route::get('/quero-vender', function () {
+    // TODO: criar layout da página
+})->name('home');
+
+Route::get('/sobre', function () {
+    // TODO: criar layout da página
+})->name('home');
+
+Route::get('/como-funciona', function () {
+    // TODO: criar layout da página
+})->name('home');
+
+Route::get('/contato', function () {
+    // TODO: criar layout da página
+})->name('home');
+
 Route::get('/painel/{vue?}', function () {
     return view('admin');
 })->where('vue', '[\/\w\.-]*')->name('dashboard');
