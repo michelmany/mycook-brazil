9e042849a699117a60df8f10d78fae4a62ecfd24
Erik Figueiredo <erik.figueiredo@gmail.com>
2017-05-13 14:28:46 -0300

    implementado busca cep rustico

--- resources/views/user/seller_register.blade.php
+++ resources/views/user/seller_register.blade.php
@@ -17,12 +17,12 @@
         </div>
 
         <div>
-            <input type="number" name="address[cep]" placeholder="CEP" value="{{old('cep')}}">
+            <input id="cep_field" type="number" name="address[cep]" placeholder="CEP" value="{{old('cep')}}">
             @include('components.error', ['field'=>'cep'])
         </div>
 
         <div>
-            <input type="text" name="address[address]" placeholder="Endereço completo" value="{{old('address')}}">
+            <input id="address_field" type="text" name="address[address]" placeholder="Endereço completo" value="{{old('address')}}">
             @include('components.error', ['field'=>'address'])
         </div>
 
@@ -37,17 +37,17 @@
         </div>
 
         <div>
-            <input type="text" name="address[neighborhood]" placeholder="Bairro" value="{{old('neighborhood')}}">
+            <input id="neighborhood_field" type="text" name="address[neighborhood]" placeholder="Bairro" value="{{old('neighborhood')}}">
             @include('components.error', ['field'=>'neighborhood'])
         </div>
 
         <div>
-            <input type="text" name="address[city]" placeholder="Município" value="{{old('city')}}">
+            <input id="city_field" type="text" name="address[city]" placeholder="Município" value="{{old('city')}}">
             @include('components.error', ['field'=>'city'])
         </div>
 
         <div>
-            <input type="text" name="address[state]" placeholder="UF" value="{{old('state')}}">
+            <input id="state_field" type="text" name="address[state]" placeholder="UF" value="{{old('state')}}">
             @include('components.error', ['field'=>'state'])
         </div>
 
@@ -62,7 +62,12 @@
         </div>
 
         <div>
-            <input type="text" name="buyer[formacao]" placeholder="Sua Formação" value="{{old('formacao')}}">
+            <label for="buyerFormacao">Formação</label>
+            <select name="buyer[formacao]" id="buyerFormacao">
+                <option disabled selected>Escolha</option>
+                <option value="formacao1">Formação 1</option>
+                <option value="formacao2">Formação 2</option>
+            </select>
             @include('components.error', ['field'=>'formacao'])
         </div>
 
@@ -88,5 +93,54 @@
 
         <input type="submit" value="Cadastrar">
     </form>
+
+    <script>
+        // TODO: criar isso com mais calma
+
+        let callApi = (url) => {
+          return new Promise((resolve, reject) => {
+            let xhr;
+            xhr = new XMLHttpRequest();
+            xhr.onload = () => {
+              if (xhr.status >= 200 && xhr.status <= 300){
+                resolve(xhr.responseText);
+              } else {
+                reject(xhr.status, xhr.statusText)
+              }
+            }
+            xhr.onerror = () => {
+              reject(xhr.status, xhr.statusText)
+            }
+            xhr.open("GET", url, true);
+            xhr.send();
+          })
+        }
+
+        let cep = document.getElementById('cep_field');
+        cep.addEventListener('change', () => {
+          if (cep.value.length !== 8) {
+            return;
+          }
+          let url = 'https://viacep.com.br/ws/' + cep.value + '/json/';
+          callApi(url)
+          .then((res) => {
+            res = JSON.parse(res);
+            let address = document.getElementById('address_field');
+            let neighborhood = document.getElementById('neighborhood_field');
+            let city = document.getElementById('city_field');
+            let state = document.getElementById('state_field');
+
+            address.value = res.logradouro;
+            neighborhood.value = res.bairro;
+            city.value = res.localidade;
+            state.value = res.uf;
+          })
+          .catch((status) => {
+            console.log('erro ' + status);
+          });
+        });
+
+
+    </script>
 
 @endsection
