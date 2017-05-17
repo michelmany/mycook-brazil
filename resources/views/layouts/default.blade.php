4d5d973cb0393e8f81e3e3ba9951a21f76ff4c13
Michel Moraes <michel.moraes@helloworldlondon.co.uk>
2017-05-16 22:39:25 -0300

    Add active class to the active page

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -20,24 +20,24 @@
                 <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
-                <a class="navbar-brand" href="#">
-                    <img class="navbar__logo" src="{{ url('assets/img/logo_white.png') }}" alt="">
+                <a class="navbar-brand" href="{{ route('sobre') }}">
+                    <img class="navbar__logo" src="{{ url('assets/img/logo_white.png') }}" alt="Logo Mycook">
                 </a>
                 <div class="collapse navbar-collapse" id="navbarCollapse">
                     <ul class="navbar-nav ml-auto">
-                        <li class="nav-item active">
+                        <li {{ (Request::is('/') ? 'class=active' : '') }}>
                             <a class="nav-link" href="{{ route('home') }}">Home</a>
                         </li>
-                        <li class="nav-item">
+                        <li {{ (Request::is('sobre') ? 'class=active' : '') }}>
                             <a class="nav-link" href="{{ route('sobre') }}">Sobre mycook</a>
                         </li>
-                        <li class="nav-item">
+                        <li {{ (Request::is('quero-vender') ? 'class=active' : '') }}>
                             <a class="nav-link" href="{{ route('queroVender') }}">Quero vender no mycook</a>
                         </li>
-                        <li class="nav-item">
+                        <li {{ (Request::is('como-funciona') ? 'class=active' : '') }}>
                             <a class="nav-link" href="{{ route('comoFunciona') }}">Como funciona</a>
                         </li>
-                        <li class="nav-item">
+                        <li {{ (Request::is('contato') ? 'class=active' : '') }}>
                             <a class="nav-link" href="{{ route('contato') }}">Contato</a>
                         </li>
                     </ul>
