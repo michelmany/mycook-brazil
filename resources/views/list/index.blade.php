35a6488397e3ce5425ca53206e486b8d65829685
Michel Moraes <michel.moraes@helloworldlondon.co.uk>
2017-05-21 18:53:12 -0300

    Frontsite - Adiciona página List

---
+++ resources/views/list/index.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.default')
+@section('content')
+
+
+    <section class="list">
+        <div class="container">
+            <h2>Em breve os melhores chefs estarão disponíveis para você!</h2>
+        </div>
+    </section>
+
+
+@endsection
