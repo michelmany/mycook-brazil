87d969d2fd273a4d1cfa8ffdebe1e75b7e1a03f1
Michel Moraes <michel.moraes@helloworldlondon.co.uk>
2017-05-29 21:58:19 -0300

    Corrige arquivos de emails e insere locale laravel pt-br

--- config/app.php
+++ config/app.php
@@ -12,7 +12,7 @@ return [
     | any other location as required by the application or its packages.
     */
 
-    'name' => 'Laraspace',
+    'name' => 'Mycook',
 
     /*
     |--------------------------------------------------------------------------
@@ -77,7 +77,7 @@ return [
     |
     */
 
-    'locale' => 'en',
+    'locale' => 'pt-br',
 
     /*
     |--------------------------------------------------------------------------
