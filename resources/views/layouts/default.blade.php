8efb58cfd87193983059ec3756c63a7226c31919
Michel Moraes <michel.moraes@helloworldlondon.co.uk>
2017-06-13 20:36:15 -0300

    Change Facebook Pixel Code

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -20,11 +20,11 @@
         n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
         t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
         document,'script','https://connect.facebook.net/en_US/fbevents.js');
-        fbq('init', '1566382683371995'); // Insert your pixel ID here.
+        fbq('init', '816561605162281'); // Insert your pixel ID here.
         fbq('track', 'PageView');
     </script>
     <noscript><img height="1" width="1" style="display:none"
-    src="https://www.facebook.com/tr?id=1566382683371995&ev=PageView&noscript=1"
+    src="https://www.facebook.com/tr?id=816561605162281&ev=PageView&noscript=1"
     /></noscript>
     <!-- End Facebook Pixel Code -->
 
