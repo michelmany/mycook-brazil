492795591815431fb98981ff458a22539ba6c115
Michel Moraes <michel.moraes@helloworldlondon.co.uk>
2017-10-06 11:25:32 +0200

    deleting view temp files

--- storage/framework/views/cd1e4ce3d317f78a4ed60c0f68c1fc223ba860e6.php
+++
@@ -1,9 +0,0 @@
-<?php if(count($errors->get($field)) > 0): ?>
-    <div class="text-danger">
-        <ul>
-            <?php $__currentLoopData = $errors->get($field); $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $error): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
-                <li><?php echo e($error); ?></li>
-            <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
-        </ul>
-    </div>
-<?php endif; ?>
