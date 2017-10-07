57ed4c873cf94088c261b9b85fa0e0ac513a47e9
Michel Moraes <michel.moraes@helloworldlondon.co.uk>
2017-10-07 12:40:53 -0300

    Reset de password para usuários

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,53 +1,61 @@
 @extends('layouts.default')
+@section('title', 'Recuperar senha')
 
 @section('content')
-<section class="hero-pages"
-        style="background-image: url('/assets/img/hero-01.jpg')">
-        <div class="hero-pages__mask"></div>
-        <div class="container">
-            <div class="hero-pages__headline ml-auto">@yield('title')</div>
+
+<div class="container generic__wrapper">
+
+    <div class="header">
+        <div>
+            <h2><i class="fa fa-unlock-alt" aria-hidden="true"></i> Recuperar senha</h2>
         </div>
-    </section
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Reset Password</div>
-                <div class="panel-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+    </div>
+    <hr>
 
-                    <form class="form-horizontal" role="form" method="POST" action="{{ route('password.email') }}">
-                        {{ csrf_field() }}
-
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
-
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Send Password Reset Link
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
+    <div class="panel panel-default">
+        <div class="panel-body">
+            @if (session('status'))
+            <div class="alert alert-success">
+                {{ session('status') }}
             </div>
+            @endif
+
+            <form class="form-horizontal" role="form" method="POST" action="{{ route('password.email') }}">
+                {{ csrf_field() }}
+
+                <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                    <label for="email" class="col-md-4 control-label">Digite seu E-mail</label>
+
+                    <div class="col-md-6">
+                        <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
+
+                        @if ($errors->has('email'))
+                        <span class="help-block">
+                            <strong>{{ $errors->first('email') }}</strong>
+                        </span>
+                        @endif
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <div class="col-md-6 col-md-offset-4">
+                        <button type="submit" class="btn btn-submit-green btn-lg">
+                            Enviar link para recuperar senha
+                        </button>
+                    </div>
+                </div>
+            </form>
         </div>
     </div>
 </div>
+
+@endsection
+
+@section('script')
+    <style>
+        .help-block {
+            font-size: 14px;
+            color: red;
+        }
+    </style>
 @endsection
