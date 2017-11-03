ebb53247a8ddc7be593391b208c28d6c3cc0c95c
Michel Moraes <michel.moraes@helloworldlondon.co.uk>
2017-11-03 16:03:52 +0100

    Amend config moip

--- config/moip.php
+++ config/moip.php
@@ -39,7 +39,7 @@ return [
     |
     */
     'marketplace' => [
-        'endpoint' => getenv('MARKETPLACE_ENDPOINT'),
+        'endpoint' => \Moip\Auth\Connect::ENDPOINT_PRODUCTION,
         'id'  => getenv('MARKETPLACE_ID'),
         'name' => 'MyCook',
         'description' => 'Delivery de comida caseira online',
