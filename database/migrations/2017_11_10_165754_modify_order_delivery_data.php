847023ea313632bfca550958ada35ff3133efc9c
Cesar Augusto <cesinhagutierres@gmail.com>
2017-11-10 16:44:59 -0300

    Correção migrate

--- database/migrations/2017_11_10_165754_modify_order_delivery_data.php
+++ database/migrations/2017_11_10_165754_modify_order_delivery_data.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class UpdateOrderDeliveryDatas extends Migration
+class ModifyOrderDeliveryData extends Migration
 {
     /**
      * Run the migrations.
@@ -15,6 +15,10 @@ class UpdateOrderDeliveryDatas extends Migration
     {
         Schema::table('order_delivery_datas', function (Blueprint $table) {
             //
+            $table->unsignedInteger('address_id');
+            $table->foreign('address_id')->references('id')->on('addresses');
+            $table->unsignedInteger('order_id');
+            $table->foreign('order_id')->references('id')->on('orders')->onDelete('cascade');
         });
     }
 
@@ -27,7 +31,8 @@ class UpdateOrderDeliveryDatas extends Migration
     {
         Schema::table('order_delivery_datas', function (Blueprint $table) {
             //
-
+            $table->dropForeign('order_delivery_datas_order_id_foreign');
+            $table->dropForeign('order_delivery_datas_address_id_foreign');
         });
     }
 }
